c1b06be62e8970f520b303c6309a5deed9e86f3f
lmh5658 <153481805+lmh5658@users.noreply.github.com>
2024-03-16 20:53:20 +0900

    fix : product.java 수정
    fix : product.java 수정
    author : 이문희
    
    product.java enroll_date -> enrollDate 수정

--- src/main/java/com/sos/product/model/vo/Product.java
+++ src/main/java/com/sos/product/model/vo/Product.java
@@ -9,15 +9,17 @@ public class Product {
 	private int price;
 	private String inventory; 
 	private Date enrollDate;
-	private Date modify_date;
+	private Date modifyDate;
 	private String status;
 	private int discountPrice;
 	private String path;
 	
 	public Product() {}
 
+	
+	
 	public Product(int productNo, String categoryNo, String productName, int price, String inventory, Date enrollDate,
-			Date modify_date, String status, int discountPrice, String path) {
+			Date modifyDate, String status, int discountPrice, String path) {
 		super();
 		this.productNo = productNo;
 		this.categoryNo = categoryNo;
@@ -25,13 +27,13 @@ public class Product {
 		this.price = price;
 		this.inventory = inventory;
 		this.enrollDate = enrollDate;
-		this.modify_date = modify_date;
+		this.modifyDate = modifyDate;
 		this.status = status;
 		this.discountPrice = discountPrice;
 		this.path = path;
 	}
-	
-	
+
+
 
 	public Product(int productNo, String categoryNo, String productName, int price, String path) {
 		super();
@@ -42,92 +44,136 @@ public class Product {
 		this.path = path;
 	}
 
+
+
 	public int getProductNo() {
 		return productNo;
 	}
+
+
 
 	public void setProductNo(int productNo) {
 		this.productNo = productNo;
 	}
 
+
+
 	public String getCategoryNo() {
 		return categoryNo;
 	}
+
+
 
 	public void setCategoryNo(String categoryNo) {
 		this.categoryNo = categoryNo;
 	}
 
+
+
 	public String getProductName() {
 		return productName;
 	}
+
+
 
 	public void setProductName(String productName) {
 		this.productName = productName;
 	}
 
+
+
 	public int getPrice() {
 		return price;
 	}
+
+
 
 	public void setPrice(int price) {
 		this.price = price;
 	}
 
+
+
 	public String getInventory() {
 		return inventory;
 	}
+
+
 
 	public void setInventory(String inventory) {
 		this.inventory = inventory;
 	}
 
+
+
 	public Date getEnrollDate() {
 		return enrollDate;
 	}
+
+
 
 	public void setEnrollDate(Date enrollDate) {
 		this.enrollDate = enrollDate;
 	}
 
-	public Date getModify_date() {
-		return modify_date;
+
+
+	public Date getModifyDate() {
+		return modifyDate;
 	}
 
-	public void setModify_date(Date modify_date) {
-		this.modify_date = modify_date;
+
+
+	public void setModifyDate(Date modifyDate) {
+		this.modifyDate = modifyDate;
 	}
+
+
 
 	public String getStatus() {
 		return status;
 	}
 
+
+
 	public void setStatus(String status) {
 		this.status = status;
 	}
+
+
 
 	public int getDiscountPrice() {
 		return discountPrice;
 	}
 
+
+
 	public void setDiscountPrice(int discountPrice) {
 		this.discountPrice = discountPrice;
 	}
+
+
 
 	public String getPath() {
 		return path;
 	}
 
+
+
 	public void setPath(String path) {
 		this.path = path;
 	}
 
+
+
 	@Override
 	public String toString() {
 		return "Product [productNo=" + productNo + ", categoryNo=" + categoryNo + ", productName=" + productName
-				+ ", price=" + price + ", inventory=" + inventory + ", enrollDate=" + enrollDate + ", modify_date="
-				+ modify_date + ", status=" + status + ", discountPrice=" + discountPrice + ", path=" + path + "]";
+				+ ", price=" + price + ", inventory=" + inventory + ", enrollDate=" + enrollDate + ", modifyDate="
+				+ modifyDate + ", status=" + status + ", discountPrice=" + discountPrice + ", path=" + path + "]";
 	}
+
+
 	
 	
 
