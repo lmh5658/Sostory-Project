f10d7760c626dded4d427c9be407714510f8c8f0
ywkwak <153481743+ywkwak@users.noreply.github.com>
2024-03-20 17:12:18 +0900

    feat: 회원목록
    author: 곽연우
    
    add : 회원 삭제 기능 추가
    add : 회원 목록 조회, 페이징 처리

--- src/main/java/com/sos/manager/model/service/ManagerService.java
+++ src/main/java/com/sos/manager/model/service/ManagerService.java
@@ -1,5 +1,43 @@
 package com.sos.manager.model.service;
 
+import java.sql.Connection;
+import java.util.List;
+
+import com.sos.common.model.vo.PageInfo;
+import com.sos.manager.model.dao.ManagerDao;
+import com.sos.member.model.vo.Member;
+
+import static com.sos.common.template.JDBCTemplate.*;
+
 public class ManagerService {
+	
+	private ManagerDao mDao = new ManagerDao();
+
+	public int selectCountList() {
+		Connection conn = getConnection();
+		int result = mDao.selectCountList(conn);
+		close(conn);
+		return result;
+	}
+
+	public List<Member> selectMemberList(PageInfo pi) {
+		Connection conn = getConnection();
+		List<Member> list = mDao.selectMemberList(conn, pi);
+		close(conn);
+		return list;
+	}
+
+	public int deleteMember(int[] userNo) {
+		Connection conn = getConnection();
+		int result = mDao.deleteMember(conn, userNo);
+		if(result > 0) {
+			commit(conn);
+		} else {
+			rollback(conn);
+		}
+		close(conn);
+		
+		return result;
+	}
 
 }
